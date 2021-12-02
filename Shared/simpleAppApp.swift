c7d662cad7c12184f9c757ea75442683d13ab8df
Maihao-zhu <mz2789@columbia.edu>
2021-12-02 13:07:23 -0500

    Initial Commit

---
+++ Shared/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  Shared
+//
+//  Created by 脉脉桑 on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
